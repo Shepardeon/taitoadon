3d3940e3bc5167a37789a620acd702ffa27461cd
Shepardeon <thomas.demaen@life-sensation.fr>
2026-04-13 20:42:41 +0200

    make client url configurable

--- client/src/services/colyseus.client.ts
+++ client/src/services/colyseus.client.ts
@@ -1,6 +1,9 @@
 import { Client } from "@colyseus/sdk";
 
 let client: Client | null = null;
+const clientUrl = import.meta.env.PROD
+  ? `${location.protocol}//${location.hostname}/api`
+  : `${location.protocol}//${location.hostname}:2567`;
 
 export function getClient() {
   if (!client) {
