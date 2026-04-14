5230f8f83be8ab5334854df884b1401c2f398028
Shepardeon <thomas.demaen@life-sensation.fr>
2026-04-14 20:52:51 +0200

    added ThemeSwitch component

---
+++ client/src/components/ThemeSwitch.vue
@@ -0,0 +1,14 @@
+<template>
+  <label class="swap swap-rotate">
+    <input type="checkbox" class="theme-controller" value="dark" />
+
+    <FontAwesomeIcon class="swap-off text-2xl fill-current" :icon="faSun" />
+
+    <FontAwesomeIcon class="swap-on text-2xl fill-current" :icon="faMoon" />
+  </label>
+</template>
+
+<script lang="ts" setup>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-regular-svg-icons";
+</script>
